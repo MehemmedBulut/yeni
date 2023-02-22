4b34cc43e6ca4ff53d01e3548f6260281fc59569
MehemmedBulut <mehemmed@mehemmed-MacBook-Air.local>
2023-02-22 10:21:29 +0400

    navbar tamamlandi

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../components/Home.css'
+//background photo
+
+
+const Home = () => {
+  return (
+    <>
+    <div className='major'>
+      <div className="container">
+      <h2>Hi,I'm</h2>
+      <h1>Mohammed Bulut.</h1>
+      <h3>Full StacK Developer</h3>
+      <div className="aboutbtn">
+        <button>ABOUT</button>
+      </div>
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+import React, {useRef} from'react'
+import '../components/Navbar.css'
+
+//react icons
+import { AiOutlineBars } from "react-icons/ai";
+import { RxCross1 } from "react-icons/rx";
+
+const Navbar = () => {
+    
+  
+    const blackDivAdressRef = useRef()
+    const openMenu = (e) => {
+    const kliklenenElement = e.target
+
+    if(kliklenenElement.classList.contains('menu-icon')){
+      blackDivAdressRef.current.classList.add('aktiv')
+    }
+  }
+
+    const closeMenu = (e) => {
+    const kliklenenElement = e.target 
+
+    if (kliklenenElement.classList.contains('cross-icon')) {
+      blackDivAdressRef.current.classList.remove('aktiv')
+      
+    }
+  }
+  
+  return (
+    
+    <>
+    <div className="overlay" ref={blackDivAdressRef} >
+        <RxCross1 className='cross-icon' onClick={closeMenu}/>
+
+        <div className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Social Media</a>
+          <a href="#">Contact</a>
+        </div>
+    </div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Məhəmməd Bulut</a>
+    <button onClick={openMenu} class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <AiOutlineBars className='menu-icon' />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Social Media</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default Navbar
