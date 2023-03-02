05acf5edc33afab079664524ff0e5bd32e73d343
MehemmedBulut <mehemmed@mehemmed-MacBook-Air.local>
2023-03-02 20:45:40 +0400

    complete major

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,29 +6,56 @@ import Plcards from './Plcards'
 
 //PLICONS
 import react from '../assets/images/react.png'
-import csharp from '../assets/images/c#.png'
+import csharp from '../assets/images/csharp.png'
 import javascript from '../assets/images/js.png'
-import htmlcss from '../assets/images/htmlcss.png'
+import css from '../assets/images/css.png'
+import htmlcss from '../assets/images/html5.png'
 import mysql from '../assets/images/mysql.png'
 import python from '../assets/images/python.jpeg'
 
 //css
 import '../components/Experience.css'
 
+//swipper configuration
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+
+
 
 const Experience = () => {
   return (
-    <>
+
+    <div className='baslik'>
+    <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+
+        className="mySwiper"
+      >
+    
+    
     <div className="king">
-        <Plcards plPhoto={htmlcss} />
-        <Plcards plPhoto={javascript} />
-        <Plcards plPhoto={react} />
-        <Plcards plPhoto={csharp} />
-        <Plcards plPhoto={mysql} />
-        <Plcards plPhoto={python} />
+        <SwiperSlide><Plcards plPhoto={javascript}/></SwiperSlide>
+        <SwiperSlide><Plcards plPhoto={react}/></SwiperSlide>
+        <SwiperSlide><Plcards plPhoto={csharp}/></SwiperSlide>
+        <SwiperSlide><Plcards plPhoto={mysql}/></SwiperSlide>
+        <SwiperSlide><Plcards plPhoto={python}/></SwiperSlide>
+    </div>
+    </Swiper>
 
     </div>
-    </>
   )
 }
 
--- src/components/Plcards.jsx
+++ src/components/Plcards.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 //css
 import '../components/Plcards.css'
 
-const Plcards = ({plPhoto}) => {
+const Plcards = ({plPhoto, classAdi}) => {
   return (
     <>
     <div className="prlanguage">
-        <img src={plPhoto} alt="" />
+        <img src={plPhoto} alt="" className={classAdi}/>
     </div>
     </>
   )
